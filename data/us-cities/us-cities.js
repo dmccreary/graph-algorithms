00c730607ff2595b38b955e405782d711e67203a
Dan Mccreary <Dan.McCreary@gmail.com>
2024-01-25 10:28:14 -0700

    Deployed 9d81832 with MkDocs version: 1.5.3

--- data/us-cities/us-cities.js
+++ data/us-cities/us-cities.js
@@ -1,7 +1,14 @@
 // read a list of cities from a JSON file
+// Create a map with labeled circles
 let cityData;
 let numCities;
 let hm = 30; // header margin
+let canvasWidth = 600;
+let canvasHeight = 400;
+let minLat;
+let maxLat;
+let minLong;
+let maxLong;
 
 function preload() {
   // Must be in the same directory as the sketch
@@ -9,31 +16,70 @@ function preload() {
 }
 
 function setup() {
-  const canvas = createCanvas(320, 440);
+  const canvas = createCanvas(canvasWidth, canvasHeight);
   var mainElement = document.querySelector('main');
   canvas.parent(mainElement);
   // we have to get the keys from the object to find the length
   numCities = Object.keys(cities).length;
-  print('Number Cities:' + numCities)
+  
+  // we need these to calculate the range maps
+  minLat = findMinMaxValue(cities, "Latitude", "min");
+  maxLat = findMinMaxValue(cities, "Latitude", "max");
+  minLong = findMinMaxValue(cities, "Longitude", "min");
+  maxLong = findMinMaxValue(cities, "Longitude", "max");
+  
+  print('Number Cities:' + numCities);
+  print(minLat, maxLat, minLong, maxLong);
   noLoop(); // No need to loop since we're just logging data
 }
 
 function draw() {
   background(220);
 
-    for (let city of cities) {
-        // Map latitude and longitude to canvas coordinates
-        let x = map(city.Latitude, -180, 180, 0, width);
-        let y = map(city.Longitude, -90, 90, height, 0);
-        let size = sqrt(city.Population) / 1000; // Adjust this scaling factor
+  for (let i=0; i<numCities; i++) {
+      // Map latitude and longitude to canvas coordinates
+      // left margin is 4, right margin is 3
+      let x = map(cities[i].Longitude, minLong-4, maxLong+3, 0, canvasWidth);
+      // top at bottom margins are both 2
+      let y = map(cities[i].Latitude, maxLat+2, minLat-2, 0, canvasHeight);
 
-        // Draw the circle for the city
-        fill(100, 100, 250, 100);
-        circle(x, y, size);
+      let size = sqrt(cities[i].Population) / 100; // Adjust this scaling factor
 
-        // Draw the city name
-        fill(0);
-        text(city["Metropolitan Area"], x + 5, y + 5); // Offset text for visibility
+      // Draw the circle for the city
+      // red, green blue and transparency
+      fill(100, 100, 250, 100);
+      circle(x, y, size);
+      // fill('white');
+      // noStroke();
+      // rect(x,y,10,10);
+
+      // Draw the city name
+      fill(0);
+      textAlign(CENTER,CENTER);
+      text(cities[i]["Name"], x, y); // Offset text for visibility
+  }
+
+}
+
+// We have an Object, not an Array
+function findMinMaxValue(data, property, metric) {
+    let values = [];
+
+    if (Array.isArray(data)) {
+        // If data is an array, use it directly
+        values = data.map(item => item[property]);
+    } else if (typeof data === 'object') {
+        // If data is an object, iterate through its properties
+        for (let key in data) {
+            if (data[key][property] !== undefined) {
+                values.push(data[key][property]);
+            }
+        }
     }
 
+    if (values.length === 0) {
+        return null;
+    }
+
+    return metric === "min" ? Math.min(...values) : Math.max(...values);
 }
